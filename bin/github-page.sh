ae8ca16e368ab1a1f967cf709260998799c1d828
Carlos <caljp13@gmail.com>
2021-04-18 13:16:18 +0200

    v5.3.0-18.Abr

--- bin/github-page.sh
+++ bin/github-page.sh
@@ -2,10 +2,10 @@
 rm -rf ./dist/leitor-de-notas/
 ng build --prod
 
-rm -f ../leitordenotas.github.io-MASTER/main.*
+# rm -f ../leitordenotas.github.io-MASTER/main.*
+# rm -f ../leitordenotas.github.io-MASTER/styles.*
 rm -f ../leitordenotas.github.io-MASTER/polyfills.*
 rm -f ../leitordenotas.github.io-MASTER/runtime.*
-rm -f ../leitordenotas.github.io-MASTER/styles.*
 
 cp -r ./dist/leitor-de-notas/* ../leitordenotas.github.io-MASTER/
 cd ../leitordenotas.github.io-MASTER/
