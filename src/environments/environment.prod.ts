9e37f41bb222e0f13ecb015a5fec8734d5963a37
Carlos <835129+cvscarlos@users.noreply.github.com>
2023-11-19 18:57:12 +0100

    chore(): novo endereço API-beta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiServer: 'https://leitor-371311.uc.r.appspot.com/',
+  apiServer: 'https://leitor-de-notas-server.rj.r.appspot.com/',
   // apiServer: 'https://api-2.leitordenotas.com.br',
 };
