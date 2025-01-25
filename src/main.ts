213d8da1dba103f8761e609aaa5fd9cc1c5b779d
Carlos <835129+cvscarlos@users.noreply.github.com>
2025-01-25 18:18:18 +0100

    chore: adiciona número da versão ao Sentry

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as Sentry from '@sentry/angular';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import packageJson from '../package.json';
 
 if (environment.production) {
   enableProdMode();
@@ -13,6 +14,7 @@ Sentry.init({
   dsn: 'https://5b8bf0c4ebaef72d93c588958ee478ae@o4505375512395776.ingest.us.sentry.io/4508327216807936',
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   environment: environment.production ? 'production' : 'development',
+  release: `${packageJson.name}@${packageJson.version}`,
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
