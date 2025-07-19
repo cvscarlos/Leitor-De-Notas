ee650cf573a8dadb24a3b03ae360e81658e64dc4
Carlos <835129+cvscarlos@users.noreply.github.com>
2025-07-19 09:24:53 +0200

    chore: add Sentry logs

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,17 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/api-5\.leitordenotas\.com\.br/],
+  tracePropagationTargets: [
+    'localhost',
+    'https://api-5.leitordenotas.com.br',
+    'https://api-upload.leitordenotas.com.br',
+  ],
   // Session Replay
   replaysSessionSampleRate: 0.2, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  _experiments: {
+    enableLogs: true,
+  },
 });
 
 platformBrowserDynamic()
