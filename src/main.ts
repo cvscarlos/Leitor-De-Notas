41cc568c08d3bf6ebba2967e0f1f5811005e45c6
Carlos <835129+cvscarlos@users.noreply.github.com>
2025-09-01 18:59:15 +0900

    fix: enable Sentry logging

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,12 @@ if (environment.isProduction) {
 
 Sentry.init({
   dsn: 'https://5b8bf0c4ebaef72d93c588958ee478ae@o4505375512395776.ingest.us.sentry.io/4508327216807936',
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.consoleLoggingIntegration({ levels: ['info', 'warn', 'error'] }),
+  ],
+  enableLogs: true,
   environment: environment.isProduction ? 'production' : 'development',
   release: `${packageJson.name}@${packageJson.version}`,
   // Tracing
@@ -26,10 +31,7 @@ Sentry.init({
   // Session Replay
   replaysSessionSampleRate: 0.2, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  _experiments: {
-    enableLogs: true,
-    beforeSendLog: (log) => (environment.isProduction ? null : log),
-  },
+  beforeSendLog: (log) => (environment.isProduction ? null : log),
   beforeSend(event, hint) {
     try {
       if (!environment.isProduction) {
