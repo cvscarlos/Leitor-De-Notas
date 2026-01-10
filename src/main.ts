6c319db946d5c919757b9b5239fba64a456bf920
Carlos <835129+cvscarlos@users.noreply.github.com>
2026-01-10 08:48:48 +0100

    chore: Angular upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import * as Sentry from '@sentry/angular';
 
@@ -57,5 +57,5 @@ Sentry.init({
 });
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch((err) => console.error(err));
